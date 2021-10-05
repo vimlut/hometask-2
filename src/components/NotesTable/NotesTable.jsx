73ec8f240d15880ae4e12a2a0ae30ea63a9a809c
Serhii Tyshchenko <vimlut@gmail.com>
2021-10-05 18:53:04 +0300

    added toggle active notes functionality

--- src/components/NotesTable/NotesTable.jsx
+++ src/components/NotesTable/NotesTable.jsx
@@ -1,19 +1,23 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { UIIconButton, NotesTableItem } from 'components';
 
 import './NotesTable.scss';
 
 const NotesTable = () => {
+  const [showActive, toggleShowActive] = useState(true);
   const dispatch = useDispatch();
   const { notes } = useSelector((state) => state);
-  const dates = (content) => content.match(/\d{1,2}\/\d{1,2}\/\d{4}/g)?.join(', ') || '';
-  const getDate = (created) => new Date(created).toLocaleDateString();
-  const contentMaxLength = 19;
-  const taskContent = (content) => (content.length > contentMaxLength ? `${content.substring(0, 19)} ...` : content);
+  const activeNotes = notes.filter((note) => !note.isArchived);
+  const archivedNotes = notes.filter((note) => note.isArchived);
+  const data = showActive ? activeNotes : archivedNotes;
+  function handleShowArchivedClick() {
+    toggleShowActive(!showActive);
+  }
 
   return (
     <table className="data-table">
@@ -24,11 +28,11 @@ const NotesTable = () => {
           <th>Category</th>
           <th>Content</th>
           <th>Dates</th>
-          <th>xx</th>
-          <th><UIIconButton icon="arch" /></th>
+          <th />
+          <th><UIIconButton icon="arch" onClick={handleShowArchivedClick} /></th>
           <th><UIIconButton icon="del" /></th>
         </tr>
-        {notes && notes.map((item) => <NotesTableItem data={item} key={item.id} />)}
+        {data.map((item) => <NotesTableItem data={item} key={item.id} />)}
       </tbody>
     </table>
   );
