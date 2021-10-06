467763b0c01607b844afad3a4be2dc86967b6e32
Serhii Tyshchenko <vimlut@gmail.com>
2021-10-06 17:10:36 +0300

    moved dummy notes to constants

--- src/store/reducers/_notes.js
+++ src/store/reducers/_notes.js
@@ -1,58 +1,10 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-confusing-arrow */
 /* eslint-disable implicit-arrow-linebreak */
+import { dummyNotes } from 'constants.js';
 import { ADD_NOTE, UPDATE_NOTE, DELETE_NOTE, DELETE_NOTES } from '../action-types';
 
-const initialState = [
-  {
-    id: 'task-1',
-    name: 'Task 1',
-    created: 1632914002039,
-    category: 'Task',
-    content: 'Lorem ipsum 5/5/2021 d awd da 6/5/2021',
-    isArchived: false,
-  },
-  {
-    id: 'task-2',
-    name: 'Task 2',
-    created: 1632914019093,
-    category: 'Random Thought',
-    content: 'Lorem ipsum',
-    isArchived: false,
-  },
-  {
-    id: 'task-3',
-    name: 'Task 3',
-    created: 1632914025810,
-    category: 'Task',
-    content: 'Lorem ipsum',
-    isArchived: false,
-  },
-  {
-    id: 'task-4',
-    name: 'Task 4',
-    created: 1632914032270,
-    category: 'Idea',
-    content: 'Lorem ipsum',
-    isArchived: false,
-  },
-  {
-    id: 'task-5',
-    name: 'Task 5',
-    created: 1632914038157,
-    category: 'Quote',
-    content: 'Lorem ipsum',
-    isArchived: true,
-  },
-  {
-    id: 'task-6',
-    name: 'Task 6',
-    created: 1632914044524,
-    category: 'Quote',
-    content: 'Lorem ipsum',
-    isArchived: true,
-  },
-];
+const initialState = dummyNotes;
 
 export const notes = (state = initialState, action) => {
   const { type, payload } = action;
